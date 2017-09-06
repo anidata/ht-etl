1ca228d81b37bc86dd499249439bf0c72c6b384e
Dan Robertson <dan.robertson@anidata.org>
2017-09-06 23:54:29 +0000

    Remove the evil whitespace
    Save space on your computer and don't add extra 0x20's

--- htetl/main_jobs.py
+++ htetl/main_jobs.py
@@ -68,7 +68,7 @@ class LoadEntityIds(util.LoadPostgres):
 
     def requires(self):
         return MakeGraph()
-    
+
 class ParseEmails(luigi.Task):
     '''
         Parses emails from raw Backpage posts & saves emails / post IDs in CSV file
@@ -79,31 +79,29 @@ class ParseEmails(luigi.Task):
     user = luigi.Parameter(significant=False)
     password = luigi.Parameter(significant=False)
     outfile = 'data/parsed_email.csv'
-    
+
     def requires(self):
         return get_data.RawHTMLPostData(self.host, self.database, self.user, self.password)
-    
+
     def output(self):
         return luigi.LocalTarget(self.outfile)
-                                                  
+
     def run(self):
         in_path = self.input().path
         logger.info("Processing {}".format(in_path))
         df = pd.read_csv(in_path)
-        '''
-            (...) = make a capture group - at least one is required for pandas Series.str.extract method
-            [\w._-] = match any alphanumeric character (\w) or . or _ or -
-            + = match preceding expression one or more times
-            @ = match @
-            \. = match .
-        '''
+        # (...) = make a capture group - at least one is required for pandas Series.str.extract method
+        # [\w._-] = match any alphanumeric character (\w) or . or _ or -
+        # + = match preceding expression one or more times
+        # @ = match @
+        # \. = match .
         df["email"] = df["body"].str.extract("([\w._-]+@[\w_-]+\.\w+)", expand=True)
         df = df.drop('body', 1)
         df = df.dropna(axis=0, how='any')
         if not df["email"].str.extract('(,)', expand=True).dropna(axis=0, how='any').empty:
-            raise ValueError(' '.join(['At least one parsed email address contains a comma,', 
+            raise ValueError(' '.join(['At least one parsed email address contains a comma,',
                              'which may cause problems when other code loads', self.outfile, 'with comma separator']))
-                            
+
         with open(self.output().path, 'a') as f:  # write posting id & emails to CSV
             df.to_csv(f, index=None, encoding='utf-8')
 
