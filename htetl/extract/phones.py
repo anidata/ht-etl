e23cd2f7144cb0c43841c9c577ab3fbe4b211ea2
elan.grossman <elangrossman@gmail.com>
2017-09-07 22:53:19 +0000

    Added phone extraction but no normalization

--- htetl/extract/phones.py
+++ htetl/extract/phones.py
@@ -1,9 +1,39 @@
+import re
 import luigi
 import logging
 import pandas as pd
 import string
 from htetl.tasks import loadpages
 from htetl import util
+
+
+logger = logging.getLogger('luigi-interface')
+logger.setLevel(logging.DEBUG)
+
+# TODO actually test and optimize the regex
+LEADING_NUMBER = '(\+?[1]?)'
+DIGIT='(\d|one|two|three|four|five|six|seven|eight|nine|zero)\s*'
+SEPARATOR = '(\s*|\s*(-|.|dash|dot)\s*)'
+TERMINATING_SEPARATOR = '(\s+|\s*(-|.|dash|dot)\s*)'
+AREA_CODE = '\(?((%s){3})\)?' % DIGIT
+FIRST_THREE = '\(?((%s){3})\)?' % DIGIT
+LAST_FOUR = '\(?((%s){4})\)?' % DIGIT
+
+# Regex variables for matching
+NUMBER_REGEX = re.compile(''.join([
+    '(',
+    LEADING_NUMBER, SEPARATOR, AREA_CODE,
+    SEPARATOR, FIRST_THREE, SEPARATOR, LAST_FOUR,
+    ')']),
+    re.IGNORECASE)
+
+SEPARATOR_REGEX = re.compile(SEPARATOR)
+
+LEADING_INDEX = 1
+AREA_CODE_INDEX = 4
+FIRST_THREE_INDEX = 9
+LAST_FOUR_INDEX = 14
+
 
 
 class ParsePhones(luigi.Task):
@@ -19,7 +49,19 @@ class ParsePhones(luigi.Task):
         return luigi.LocalTarget(self.outfile)
 
     def run(self):
+	in_path = self.input().path
+        logger.info("Processing {}".format(in_path))
+        df = pd.read_csv(in_path)
+        phones = []
+        # go over each html and grap phones
+        for i, row in df.iterrows():
+            match = NUMBER_REGEX.findall(row["content"])
 
-        with open(self.output().path, 'a') as f:  
-	    # write posting id & phones to CSV
-            f.write("MEOW")
+            if match:
+                for phone in match:
+                    phones.append([row["id"],phone[0].strip()])
+
+        phonedf = pd.DataFrame(phones)
+        with open(self.output().path, 'a') as f:
+        # write posting id & phones to CSV
+            phonedf.to_csv(f,index=False)
