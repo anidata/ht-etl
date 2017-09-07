b30c071933e11696ae5895bee03056f5998d6a89
Bryant Menn <bryant.menn@gmail.com>
2017-09-06 21:51:41 -0400

    Create url extractor for arbitrary text

--- htetl/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from . import util
-from . import get_data
-from . import main_jobs
-from . import reverse_url
-
--- tests/extract/test_sites.py
+++ tests/extract/test_sites.py
@@ -1,7 +1,17 @@
 import htetl.extract.sites as sites
 
 def test_extract_urls():
-    expected = 'http://www.google.com'
-    result = sites.extract_urls(expected)
+    data = [
+        ('some text http://www.google.com',
+         ['http://www.google.com']),
+        ('foo https://somewhere.org/?foo=bar',
+         ['https://somewhere.org/?foo=bar']),
+        ('http://elsewhere.net/?bar=foo https://place.net/',
+         ['http://elsewhere.net/?bar=foo', 'https://place.net/'])
+    ]
+    for text, expected in data:
+        yield check_extract_urls, text, expected
 
-    assert result == [expected]
+def check_extract_urls(text, expected):
+    result = sites.extract_urls(text)
+    assert result == expected
