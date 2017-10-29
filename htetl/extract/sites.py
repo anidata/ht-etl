bf9cbcb6fa8b583faa92020ff4c7d927a149ec01
Bryant Menn <bryant.menn@gmail.com>
2017-10-29 00:43:36 -0400

    Replace rfc3987 library for parsing URLs with manual regex
    rfc3987 library expects strict adherence to the URL specificiation,
    which requires the protocol to be specified. There might be a better
    middle ground.

--- htetl/extract/sites.py
+++ htetl/extract/sites.py
@@ -1,6 +1,37 @@
-import rfc3987
+import re
 
-URL_REGEX=rfc3987.get_compiled_pattern('%(URI)s')
+# Old rfc3987 library based regex does not cover what is human-intepreted as
+# a URL (e.g. www.google.com)
+# Not sure if ftp and sftp are necessary
+PROTOCOLS = r'((http|https|ftp|sftp)://)'
+
+SUBDOMAIN = r'([a-z0-9]+\.)'
+DOMAIN_NAME = r'([a-z0-9]+)'
+DOMAIN = r'((\.[a-z]{2,3})?\.[a-z]{2,4})'
+HOST = SUBDOMAIN + '?' + DOMAIN_NAME + DOMAIN
+
+PORT = r'(:[0-9]{0,6})'
+AUTHORITY = '(' + HOST + PORT + '?)'
+
+URL_REGEX = re.compile(
+    ''.join([
+        '(',
+        PROTOCOLS, '?',
+        AUTHORITY, '/?',
+        '(\S+)?',
+        ')',
+    ]),
+    re.IGNORECASE
+)
+
+SITE_REGEX = re.compile(
+    ''.join([
+        PROTOCOLS, '?',
+        SUBDOMAIN, '*',
+        '(', DOMAIN_NAME, DOMAIN, PORT, '?)',
+    ]),
+    re.IGNORECASE
+)
 
 def extract_urls(text):
     '''Extract urls from given text
@@ -10,7 +41,7 @@ def extract_urls(text):
 
     '''
 
-    return URL_REGEX.findall(text)
+    return [m[0] for m in URL_REGEX.findall(text)]
 
 
 def extract_sites(text):
@@ -19,13 +50,6 @@ def extract_sites(text):
     :text: (str) Text to extract sites from
     :returns: List of sites extracted
     '''
-    urls = URL_REGEX.findall(text)
-    sites = set(
-        # Take only the last part of the authority
-        '.'.join(
-            rfc3987.parse(u, rule='URI')['authority'].split('.')[-2:]
-        )
-        for u in urls
-    )
+    sites = [m[3] for m in SITE_REGEX.findall(text)]
 
-    return list(sites)
+    return sites
