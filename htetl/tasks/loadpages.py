db2796c9a8db6c1fbce2c069ca5a683f960a9937
Bryant Menn <bryant.menn@gmail.com>
2017-11-11 00:47:22 -0500

    Remove page query return limit set while testing

--- htetl/tasks/loadpages.py
+++ htetl/tasks/loadpages.py
@@ -3,7 +3,7 @@ from  htetl import util
 
 class RawPageData(util.QueryPostgres):
     """ load eaxmple """
-    sql = 'select id, content from page limit 100'
+    sql = 'select id, content from page'
 
     def output(self):
         in_path = 'data/raw_page.csv'
